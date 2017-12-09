a78d4af2b8387cf49f8861391fcdc710014dd470
Mariano Fernández López <mariano@informaticaemprendimiento.org>
2017-12-09 13:04:21 +0100

    Procesa una entrada de bibtex individual

--- desarrollo/diccionario_bib.py
+++ desarrollo/diccionario_bib.py
@@ -1,73 +1,18 @@
-@inicio{regimenProfesorado,
-    title = {Real Decreto 898/1985, de 30 de abril, sobre régimen del profesorado universitario},
-    year = {1985},
-    note = "El documento consolidado está disponible, con la última
-        modificación de 22 de enero de 2000, está disponible en
-        \url{https://www.boe.es/buscar/pdf/1985/BOE-A-1985-11578-consolidado.pdf}"
-fin}
+@inicio{regimenProfesorado, title = {Real Decreto 898/1985, de 30 de abril, sobre régimen del profesorado universitario}, year = {1985}, note = "El documento consolidado está disponible, con la última modificación de 22 de enero de 2000, está disponible en url{https://www.boe.es/buscar/pdf/1985/BOE-A-1985-11578-consolidado.pdf}" fin}
 
-@inicio{rdUniversidadesPrivadas,
-    title = {Real Decreto 557/1991, de 12 de abril, sobre creación y
-        reconocimiento de universidades y centros universitarios}, year =
-        {1991},
-    note = "El documento está disponible en
-        \url{https://www.boe.es/buscar/pdf/1985/BOE-A-1985-11578-consolidado.pdf}"
-fin}
+@inicio{rdUniversidadesPrivadas, title = {Real Decreto 557/1991, de 12 de abril, sobre creación y reconocimiento de universidades y centros universitarios}, year = {1991}, note = "El documento está disponible en url{https://www.boe.es/buscar/pdf/1985/BOE-A-1985-11578-consolidado.pdf}" fin}
 
-@inicio{leyUniversidades,
-    title = {Ley Orgánica 6/2001, de 21 de diciembre, de Universidades},
-    year = {2016},
-    note = "El documento consolidado está disponible, con la última
-        modificación de 23 de marzo de 2016, está disponible en
-        \url{http://www.boe.es/buscar/pdf/2001/BOE-A-2001-24515-consolidado.pdf}"
-fin}
+@inicio{leyUniversidades, title = {Ley Orgánica 6/2001, de 21 de diciembre, de Universidades}, year = {2016}, note = "El documento consolidado está disponible, con la última modificación de 23 de marzo de 2016, está disponible en url{http://www.boe.es/buscar/pdf/2001/BOE-A-2001-24515-consolidado.pdf}" fin}
 
-@inicio{rdECTS,
-    title = {Real Decreto 1125/2003, de 5 de septiembre, por el que se
-        establece el sistema europeo de créditos y el sistema de califica-
-            ciones en las titulaciones universitarias de carácter oficial y
-            validez en todo el territorio nacional. },
-            year = {2003}, 
-            note = "El documento está disponible en 
-        \url{https://www.boe.es/boe/dias/2003/09/18/pdfs/A34355-34356.pdf}"
-fin}
+@inicio{rdECTS, title = {Real Decreto 1125/2003, de 5 de septiembre, por el que se establece el sistema europeo de créditos y el sistema de califica- ciones en las titulaciones universitarias de carácter oficial y validez en todo el territorio nacional. }, year = {2003}, note = "El documento está disponible en url{https://www.boe.es/boe/dias/2003/09/18/pdfs/A34355-34356.pdf}" fin}
 
-@inicio{rd1393,
-    title = {Real Decreto 1393/2007, de 29 de octubre, por el que se establece
-        la ordenación de las enseñanzas universitarias oficiales.}, 
-    year = {2007}, 
-            note = "El documento está disponible en 
-        \url{https://www.boe.es/boe/dias/2007/10/30/pdfs/A44037-44048.pdf}"
-fin}
+@inicio{rd1393, title = {Real Decreto 1393/2007, de 29 de octubre, por el que se establece la ordenación de las enseñanzas universitarias oficiales.}, year = {2007}, note = "El documento está disponible en url{https://www.boe.es/boe/dias/2007/10/30/pdfs/A44037-44048.pdf}" fin}
 
-@inicio{estatutoEstudiante,
-    title = {Real Decreto 1791/2010, de 30 de diciembre, por el que se aprueba
-        el Estatuto del Estudiante Universitario.}, year = {2010},
-    note = "El documento está disponible en
-        \url{https://www.boe.es/boe/dias/2010/12/31/pdfs/BOE-A-2010-20147.pdf}"
-fin}
+@inicio{estatutoEstudiante, title = {Real Decreto 1791/2010, de 30 de diciembre, por el que se aprueba el Estatuto del Estudiante Universitario.}, year = {2010}, note = "El documento está disponible en url{https://www.boe.es/boe/dias/2010/12/31/pdfs/BOE-A-2010-20147.pdf}" fin}
 
-@inicio{rd861,
-    title = {Real Decreto 861/2010, de 2 de julio, por el que se modifica el Real Decreto
-        1393/2007, de 29 de octubre, por el que se establece la ordenación de las enseñanzas universitarias oficiales.},
-    year = {2010},
-    note = "El documento está disponible en
-        \url{https://www.boe.es/boe/dias/2010/07/03/pdfs/BOE-A-2010-10542.pdf}"
-fin}
+@inicio{rd861, title = {Real Decreto 861/2010, de 2 de julio, por el que se modifica el Real Decreto 1393/2007, de 29 de octubre, por el que se establece la ordenación de las enseñanzas universitarias oficiales.}, year = {2010}, note = "El documento está disponible en url{https://www.boe.es/boe/dias/2010/07/03/pdfs/BOE-A-2010-10542.pdf}" fin}
 
-@inicio{ guiaAneca,
-       title = {Guía de apoyo para la elaboración de la memoria de verificación
-           de títulos oficialaes universitarios (Grado y Máster) },
-       publisher = {Agencia Nacional de Evaluación de la Calidad y Acreditación (ANECA)},
-       year = {2012},
-       note = "Disponible en \url{http://www.aneca.es/content/download/12155/136031/file/verifica_gm_guia_V05.pdf}"
-fin}
+@inicio{ guiaAneca, title = {Guía de apoyo para la elaboración de la memoria de verificación de títulos oficialaes universitarios (Grado y Máster) }, publisher = {Agencia Nacional de Evaluación de la Calidad y Acreditación (ANECA)}, year = {2012}, note = "Disponible en url{http://www.aneca.es/content/download/12155/136031/file/verifica_gm_guia_V05.pdf}" fin}
 
-@inicio{ guiaMadridMasD,
-       title = {Guía práctica para la
-           evaluación del seguimiento},
-       publisher = {Fundación Madri+d para el conocimiento},
-       year = {2014},
-       note = "Disponible en \url{http://www.madrimasd.org/uploads/acreditacion/doc/seguimiento2013/Guia_de_Evaluacion_2014.pdf}"
-fin}
+@inicio{ guiaMadridMasD, title = {Guía práctica para la evaluación del seguimiento}, publisher = {Fundación Madri+d para el conocimiento}, year = {2014}, note = "Disponible en url{http://www.madrimasd.org/uploads/acreditacion/doc/seguimiento2013/Guia_de_Evaluacion_2014.pdf}" fin}
 
--- desarrollo/generate_owl.py
+++ desarrollo/generate_owl.py
@@ -2,14 +2,65 @@
 
 import re
 import unicodedata
+from collections import namedtuple
 
-def eliminar_instruccion_url(instruccion_url):
+EntradaBibTex = namedtuple("EntradaBibTex", "identificador titulo uri")
+
+#:%s@]{leyUniversidades}@ del Real Decreto 898/1985, sobre régimen del profesorado universitario 00https://www.boe.es/buscar/pdf/1985/BOE-A-1985-11578-consolidado.pdf00]@g
+
+'''
+def generar_ontologia(datos_leidos):
+    with open('cabecera.owl') as f:
+         cabecera = f.read()
+         f.closed
+    with open('ontologia.owl', 'w') as g:
+         g.write(cabecera)
+         etiquetas = obtener_anotaciones(datos_leidos, obtener_etiqueta, -1)
+         itEt = iter(etiquetas)
+         definiciones = obtener_anotaciones(datos_leidos, obtener_definicion, -3)
+         itDef = iter(definiciones)
+         print('Número de etiquetas generadas: ' + str(len(etiquetas)))
+         print('Número de definiciones generadas: ' + str(len(definiciones)))
+         try:
+             for i in range(len(etiquetas)):
+                 etiqueta = next(itEt)
+                 termino = generar_uri(etiqueta)
+                 g.write('<!--' +  termino + '-->\n')
+                 g.write('<owl:Class rdf:about="'+termino+'">\n')
+                 g.write('   <rdfs:comment rdf:datatype="http://www.w3.org/2001/XMLSchema#string">'+next(itDef)+'</rdfs:comment>\n')
+                 g.write('   <rdfs:label xml:lang="es">'+etiqueta+'</rdfs:label>\n')
+                 g.write('</owl:Class>\n\n')
+         except StopIteration:
+              pass
+         g.write('</rdf:RDF>')
+         g.closed
+'''
+
+def generar_referencia(definicion, entrada_bibtex):
+    #entrada_bibtex = leer_string_entrada_bibtex(string_entrada_bibtex)
+    linea = definicion
+    linea = re.sub('\[', '[véase el ', linea)
+    linea = re.sub(']{'+entrada_bibtex.identificador+'}', ' del ' + entrada_bibtex.titulo + ' 00(' + entrada_bibtex.uri + ')00', linea)
+    #print(']{'+entrada_bibtex.identificador+'}')
+    #linea = re.sub(']{'+entrada_bibtex.identificador+'}', ' xxx  ', linea)
+    linea = re.sub('\[','(', linea)
+    linea = re.sub('\]',')', linea)
+    linea = re.sub('00\(','[', linea)
+    linea = re.sub('\)00', '])', linea)
+    print(linea)
+    return linea
+
+def obtener_uri_de_latex(instruccion_url):
     return re.search('(?<=url{).+?}', instruccion_url).group()[:-1] # La última interrogación se utiliza para indicare que es non-greedy
 
-def obtener_seeAlso_una_entidad(entrada_bibtex):
-    titulo = re.search('(?<=title = {).+?}', entrada_bibtex).group()[:-1] # La última interrogación se utiliza para indicare que es non-greedy
-    nota = re.search('(?<=note = {).+?}', entrada_bibtex).group()[:-1] # La última interrogación se utiliza para indicare que es non-greedy
-    return titulo + nota  
+def leer_string_entrada_bibtex(string_entrada_bibtex):
+    print(string_entrada_bibtex)
+    identificador = re.search('(?<=@inicio{).+?,', string_entrada_bibtex).group()[:-1]
+    titulo = re.search('(?<=title = {).+?}', string_entrada_bibtex).group()[:-1] # La última interrogación se utiliza para indicare que es non-greedy
+    nota = re.search('(?<=note = ").+?"', string_entrada_bibtex).group()[:-1] # La última interrogación se utiliza para indicare que es non-greedy
+    uri = obtener_uri_de_latex(nota)
+    ebt = EntradaBibTex(identificador, titulo, uri)
+    return ebt 
 
 #def obtener_seeAlso_ontologia():
 
@@ -73,8 +124,7 @@ def obtener_definicion(cadena):
     m = re.search('(?<=<<<).+?>>>', cadena)
     return m
 
-with open('glosario_2.tex') as f:
+with open('glosario_mod_1.tex') as f:
     datos_leidos = f.read()
     f.closed
-#print(eliminar_instruccion_url('url{https://www.boe.es/buscar/pdf/1985/BOE-A-1985-11578-consolidado.pdf}'))
-generar_ontologia(datos_leidos)
+#generar_ontologia(datos_leidos)
